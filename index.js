032cb819fb07a7c438b4348f88d01f33b9a3c79a
Gorresubbarao <gsubbu2109@gmail.com>
2024-06-12 08:39:14 +0000

    react js cp-1

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  //   Write your code here.
+  <div className='container'>
+    <h1 className='heading'>Super Over League</h1>
+    <div className='img-container'>
+      <img
+        src='https://assets.ccbp.in/frontend/react-js/rcb-img.png'
+        className='img1'
+      />
+      <img
+        src='https://assets.ccbp.in/frontend/react-js/csk-img.png'
+        className='img1'
+      />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
